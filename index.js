f2a8942d1bcaf8fadf4933eec5053b9d825968b3
风痕 <inkedawn@gmail.com>
2019-05-09 22:43:37 +0800

    electron窗口大小

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 900,
+    width: 1240,
+    height: 700,
     webPreferences: {
       nodeIntegration: true
     }
